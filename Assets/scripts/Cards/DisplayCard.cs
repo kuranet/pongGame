1da64a5fa2586630acc5f13aeceea3761cb554a3
Ann <anna.kurennaya38@gmail.com>
2020-06-16 13:44:40 +0300

    Flip card
    change card's layers when it is activated

---
+++ Assets/scripts/Cards/DisplayCard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayCard : MonoBehaviour
+{
+    public CardInfo card;
+
+    public TextMeshProUGUI description;
+    public Animation animation;
+    public TextMeshProUGUI time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
