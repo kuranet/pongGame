1c88207538676e9d55d4ec71a2c9e0ea8f34e591
Ann <anna.kurennaya38@gmail.com>
2020-06-24 12:16:14 +0300

    Change ball movement from translate to rb2d, add speed every delta time

--- Assets/scripts/Ball.cs
+++ Assets/scripts/Ball.cs
@@ -4,74 +4,82 @@ using System.Collections.Generic;
 public class Ball : MonoBehaviour
 {
     public bool waiting = true;
-    Vector2 direction; // direction ball moving
+   // Vector2 direction; // direction ball moving
     float speed = 0.1f; // moving speed
     int previousMiss = 0; // number of player who previous miss the ball
+    Rigidbody2D rb2d;
     // Spawn ball at the start of our game
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
         RespawnBall();
     }
     
 
     void FixedUpdate()
     {
-        if (!waiting)
-        {
-            // Don't start the game if player hasn't started it
-            if (Input.GetKey(KeyCode.Space))
-                gameObject.tag = "Moving";
-            // Move the ball every frame
-            if (gameObject.CompareTag("Moving"))
-            {
-                transform.Translate(direction * speed);
-                Debug.Log("X : " + direction.x + "  Y : " + direction.y);
-            }
-        }
+        //if (!waiting)
+        //{
+        // Don't start the game if player hasn't started it
+        //if (Input.GetKey(KeyCode.Space))
+        //    gameObject.tag = "Moving";
+        //// Move the ball every frame
+        //if (gameObject.CompareTag("Moving"))
+        //{
+        float speed = Mathf.Sqrt(Mathf.Pow(rb2d.velocity.x, 2) + Mathf.Pow(rb2d.velocity.y, 2));
+        if(speed<50)
+            rb2d.velocity += 3* Time.deltaTime*rb2d.velocity/1000;
+        Debug.Log("X : " + rb2d.velocity.x + "  Y : " + rb2d.velocity.y);
+            //}
+        //}
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         float collisionAngle = collision.gameObject.transform.rotation.eulerAngles.z * Mathf.PI / 180;
         
-        Vector3 collisionRotation = new Vector3(Mathf.Cos(collisionAngle),0,0);
-        if (collisionAngle < Mathf.PI)
-            collisionRotation.y = Mathf.Sqrt(1 - Mathf.Pow(collisionRotation.x, 2));
-        else
-            collisionRotation.y = -Mathf.Sqrt(1 - Mathf.Pow(collisionRotation.x, 2)); 
-
-        //Debug.Log("New collision vector is:  " + collisionRotation.x + "  ,  " + collisionRotation.y);
+        Vector3 collisionRotation = new Vector3(Mathf.Cos(collisionAngle),Mathf.Sin(collisionAngle),0);
+        
+        Debug.Log("New collision vector is:  " + collisionRotation.x + "  ,  " + collisionRotation.y);
         
 
         switch (collision.gameObject.tag)
         {
-            case "Player":
-            case "AI":
-            case "IntermediateWall":
-                {
-                    float angle = Mathf.Acos((direction.x* collisionRotation.x + direction.y* collisionRotation.y)/Mathf.Sqrt(Mathf.Pow(direction.x,2)+ Mathf.Pow(direction.y, 2)));
-                    
-                    if ((angle > (Mathf.PI - Mathf.PI / 18) && angle < (Mathf.PI  + Mathf.PI / 18)))
-                    {
-                        //Debug.Log(direction.y);
-                        angle = (Mathf.PI - angle) * 2 + Mathf.PI / 36;
-                        if (direction.y*direction.x >= 0)
-                        {
-                        direction.x = -(direction.x * Mathf.Cos(angle) - direction.y * Mathf.Sin(angle));
-                        direction.y = -(direction.x * Mathf.Sin(angle) + direction.y * Mathf.Cos(angle));
-                        }
-                        else
-                        {
-                            direction.x = -(direction.x * Mathf.Cos(-angle) - direction.y * Mathf.Sin(-angle));
-                            direction.y = -(direction.x * Mathf.Sin(-angle) + direction.y * Mathf.Cos(-angle));
-                        }
-                    }
-                    
-                    else
-                    {
-                        direction = Vector3.Reflect(direction, collisionRotation);
-                    }
-                    break;
-                }
+            //case "Player":
+            //case "AI":
+            //case "IntermediateWall":
+            //    {
+            //        float angle = Mathf.Acos((rb2d.velocity.x * collisionRotation.x + rb2d.velocity.y * collisionRotation.y) / Mathf.Sqrt(Mathf.Pow(rb2d.velocity.x, 2) + Mathf.Pow(rb2d.velocity.y, 2)));
+            //        Debug.Log("Angle : " + angle * 180 / Mathf.PI);
+            //        if ((angle > (Mathf.PI - Mathf.PI / 18) && angle < (Mathf.PI + Mathf.PI / 18)))
+            //        {
+            //            Debug.LogWarning("Change direction!");
+            //            angle = (Mathf.PI - angle) * 2 + Mathf.PI / 36;
+            //            if (rb2d.velocity.y * rb2d.velocity.x >= 0)
+            //            {
+            //                Debug.LogWarning("X before : " + rb2d.velocity.x);
+            //                Debug.LogError("X first : " + rb2d.velocity.x * Mathf.Cos(angle));
+            //                Debug.LogError("X second : -" + rb2d.velocity.y * Mathf.Sin(angle));
+            //                Debug.LogWarning("X : " + (rb2d.velocity.x * Mathf.Cos(angle) - rb2d.velocity.y * Mathf.Sin(angle)));
+            //                Debug.LogWarning("Y before : " + rb2d.velocity.y);
+            //                Debug.LogError("Y first : " + rb2d.velocity.x * Mathf.Sin(angle));
+            //                Debug.LogError("Y second : " + rb2d.velocity.y * Mathf.Cos(angle));
+            //                Debug.LogWarning("Y : " + (rb2d.velocity.x * Mathf.Sin(angle) + rb2d.velocity.y * Mathf.Cos(angle)));
+            //                rb2d.velocity = new Vector2(-(rb2d.velocity.x * Mathf.Cos(angle) - rb2d.velocity.y * Mathf.Sin(angle)), rb2d.velocity.x * Mathf.Sin(angle) + rb2d.velocity.y * Mathf.Cos(angle));
+
+            //            }
+            //            else
+            //            {
+            //                rb2d.velocity = new Vector2(-(rb2d.velocity.x * Mathf.Cos(-angle) - rb2d.velocity.y * Mathf.Sin(-angle)), rb2d.velocity.x * Mathf.Sin(-angle) + rb2d.velocity.y * Mathf.Cos(-angle));
+
+            //            }
+            //        }
+
+            //        else
+            //        {
+            //            rb2d.velocity = -Vector3.Reflect(rb2d.velocity, collisionRotation);
+            //        }
+            //        break;
+            //    }
             case "GameOverWall":
                 {
                     int indexOfPlayer = collision.gameObject.GetComponent<WallOvner>().Ovner.data.numberOfPlayer;
@@ -96,19 +104,20 @@ public class Ball : MonoBehaviour
     }
     private void RespawnBall()
     {
-        //Debug.Log("Previous is : " + previousMiss);
-        // Starter position of the ball
+        ////Debug.Log("Previous is : " + previousMiss);
+        //// Starter position of the ball
         transform.position = new Vector3(-2.75f, 0, 0);
         // Randomly choose the direction of the ball
         float n = EventManager.Instance.Players.Count; // number of segments
         n = 2 * Mathf.PI / n; // segment angle representation
         //Debug.Log("Max angle of segment : " + n);
-        float angle = (Mathf.PI / 2 - n + Mathf.PI / 2)/2; // generate random angle from 5 to n degrees
+        float angle = (Mathf.PI / 2 - n + Mathf.PI / 2) / 2; // generate random angle from 5 to n degrees
         angle = Random.Range(angle - Mathf.PI / 6, angle + Mathf.PI / 6);
-        Debug.Log("Angle is : " + angle*180/Mathf.PI);
+        Debug.Log("Angle is : " + angle * 180 / Mathf.PI);
         angle -= previousMiss * n;
 
-        direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-        Debug.Log("Direction vector : " + direction.x + "  ,  " + direction.y);
+        rb2d.velocity = 3*new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        //rb2d.velocity = 2*new Vector2(0.8721476f, 0.4892429f);
+        Debug.Log("Direction vector : " + rb2d.velocity.x + "  ,  " + rb2d.velocity.y);
     }
 }
--- Assets/scripts/PlayerAI.cs
+++ Assets/scripts/PlayerAI.cs
@@ -2,18 +2,25 @@
 
 public class PlayerAI : Player
 {
-    
+    Rigidbody2D rb2d;
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
     //public float dY;
     public bool shoulBeStopped = false;
     public override void FixedUpdate()
     {
-        Vector3 ballPosition = FindObjectOfType<Ball>().transform.position;
+        Vector3 ballPosition = FindObjectOfType<Ball>().gameObject.transform.position;
+        //Debug.LogWarning("X: " + ballPosition.x + "  Y: " + ballPosition.y);
+        //Debug.LogError("X: " + transform.position.x + "  Y: " + transform.position.y);
         float distance = Mathf.Sqrt(Mathf.Pow(transform.position.x - ballPosition.x, 2) + Mathf.Pow(transform.position.y - ballPosition.y, 2));
 
         if (!shoulBeStopped)
             if (distance <= 4.5f)
             {
-                float dY = transform.position.y - ballPosition.y;
+                float dY = ballPosition.y - transform.position.y;
+                //Debug.LogWarning("dY : " + dY);
                 if (dY != 0)
                     dY /= Mathf.Abs(dY);
                 if (data.shouldmove)
@@ -26,11 +33,11 @@ public class PlayerAI : Player
                 {
                     Vector3 pos = transform.position;
                     if (dY * (sideOfCollisison - transform.position.y) > 0)
-                        transform.Translate(0,  1.5f*dY * PlayersData.SPEED * Time.deltaTime, 0);
-                    else
                         transform.Translate(0, -1.5f * dY * PlayersData.SPEED * Time.deltaTime, 0);
+                    else
+                        transform.Translate(0, 1.5f * dY * PlayersData.SPEED * Time.deltaTime, 0);
                     transform.position = Vector3.Lerp(transform.position, pos, 0.5f);
-                                        
+
                 }
                 //Debug.Log(transform.position);
             }
